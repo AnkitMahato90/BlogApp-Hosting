f17e22ebb75f784632da6858703796fa1510a21f
AnkitMahato90 <ankit100mahato@gmail.com>
2024-08-18 17:11:16 +0530

    vite update

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://ankit-blog.onrender.com',
         secure: false,
       },
     },
